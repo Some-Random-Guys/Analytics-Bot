ad867a2927987bd2afc84761903f9c3162b4c7c8
Raj Dave <10864@bpsdoha.edu.qa>
2023-06-09 11:31:27 +0300

    many minor improvements

--- cogs/Main.py
+++ cogs/Main.py
@@ -12,24 +12,25 @@ from backend import log, embed_template, error_template
 class Main(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.db: DB = DB(db_creds)
 
     @commands.Cog.listener()
     async def on_ready(self):
         log.info("Cog: Main.py Loaded")
         # sync commands
-        # await self.client.tree.sync()
+        await self.client.tree.sync()
 
     @app_commands.command()
-    async def wordcloud_(self, interation, user_id: discord.Member):
-        cloud = await wordcloud(self.db, interation.guild.id, user_id.id)
+    async def wordcloud_(self, interation, member: discord.Member):
+        await interation.response.defer()
+        db: DB = DB(db_creds)
+        cloud = await wordcloud(db, interation.guild.id, member.id)
 
         embed = embed_template()
         embed.title = "Word Cloud"
-        embed.description = f"Here is the wordcloud for {'<@' + str(user_id) + '>' if user_id else 'this server'}"
+        embed.description = f"Here is the wordcloud for {member.mention if member else 'this server'}"
         embed.set_image(url="attachment://image.png")
 
-        await interation.response.send(embed=embed, file=discord.File(cloud, filename="image.png"))
+        await interation.followup.send(embed=embed, file=discord.File(cloud, filename="image.png"))
 
         os.remove(cloud)
 
@@ -45,18 +46,18 @@ class Main(commands.Cog):
     ])
     async def top(self, interaction, type_: app_commands.Choice[str], category: app_commands.Choice[str],
                   amount: int = 10):
-
         await interaction.response.defer()
+        db = DB(db_creds)
 
         embed = embed_template()
-        embed.title = f"Top {amount} {type_}"
+        embed.title = f"Top {amount} {type_.name}s"
 
         if type_.value == "channel":
-            res = await get_top_channels_visual(self.db, interaction.guild.id, self.client, category.value, amount)
+            res = await get_top_channels_visual(db, interaction.guild.id, self.client, category.value, amount)
             embed.description = f"Top {amount} channels in this guild"
 
         elif type_.value == "user":
-            res = await get_top_users_visual(self.db, interaction.guild.id, self.client, category.value, amount)
+            res = await get_top_users_visual(db, interaction.guild.id, self.client, category.value, amount)
             embed.description = f"Top {amount} users in this guild"
 
         embed.set_image(url="attachment://image.png")
