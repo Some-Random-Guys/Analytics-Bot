b258b40c2fbd8461cf419ea7ce87cacb3c0ca9e3
yashasviallen <yashasviallen@gmail.com>
2024-01-02 21:56:45 +0530

    Fixes to bugged archive cmd

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -171,7 +171,16 @@ class Admin(commands.GroupCog, name="admin"):
             elif type(channel) == discord.ForumChannel:
                 for thread in channel.threads:
                     await process_channel(thread)
+                tasks = []
+                async for thread in channel.archived_threads():
+                    if not thread.permissions_for(interaction.guild.me).read_messages:
+                        embed = error_template(
+                            f"Missing permissions to read messages in {thread.mention}"
+                        )
+                        await cmd_channel.send(embed=embed)
 
+                    tasks.append(process_channel(thread))
+                await asyncio.gather(*tasks)
         else:
             tasks = []
             # for each text channel
@@ -191,7 +200,7 @@ class Admin(commands.GroupCog, name="admin"):
                         await cmd_channel.send(embed=embed)
 
                     tasks.append(process_channel(thread))
-                async for thread in channel.archived_threads(private=True):
+                async for thread in channel.archived_threads():
                     if not thread.permissions_for(interaction.guild.me).read_messages:
                         embed = error_template(
                             f"Missing permissions to read messages in {thread.mention}"
@@ -210,7 +219,7 @@ class Admin(commands.GroupCog, name="admin"):
                         await cmd_channel.send(embed=embed)
 
                     tasks.append(process_channel(forum))
-                async for thread in channel.archived_threads(private=True):
+                async for thread in channel.archived_threads():
                     if not thread.permissions_for(interaction.guild.me).read_messages:
                         embed = error_template(
                             f"Missing permissions to read messages in {thread.mention}"
