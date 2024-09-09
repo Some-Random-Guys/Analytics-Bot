c6fabbf1e41d0b6eccd923006a4d90e14254aa7e
Raj Dave <rajdave8002@gmail.com>
2024-09-09 15:58:25 +0300

    get working /profile

--- cogs/Main.py
+++ cogs/Main.py
@@ -112,32 +112,29 @@ class Main(commands.Cog):
             member = interaction.user
 
         # Get the file from package
-        profile: Profile = await build_profile(
+        profile = await build_profile(
             db=db, guild_id=interaction.guild.id, user_id=member.id
         )
 
         # Create embed
         embed = embed_template()
         embed.title = f"Profile for {member}"
-        embed.description = f"Here is the profile for {member.mention}. Took {round(profile.time_taken, 2)} seconds to generate."
+        embed.description = f"Here is the profile for {member.mention}. Took {round(profile['time_taken'], 2)} seconds to generate."
 
-        embed.add_field(name="Messages", value=profile.messages, inline=False)
-        embed.add_field(name="Words", value=profile.words, inline=False)
-        embed.add_field(name="Characters", value=profile.characters, inline=False)
+        embed.add_field(name="Messages", value=profile['messages'], inline=False)
+        embed.add_field(name="Characters", value=profile['characters'], inline=False)
         embed.add_field(
             name="Average Message Length",
-            value=f"{round(profile.average_msg_length, 2)} Characters",
+            value=f"{round(profile['average_message_length'], 2)} Characters",
             inline=False,
         )
-        embed.add_field(name="Top Words", value=str(profile.top_words), inline=False)
         embed.add_field(
-            name="Total Attachments", value=profile.total_attachments, inline=False
+            name="Total Attachments", value=profile['total_attachments'], inline=False
         )
         if member.bot:
             embed.add_field(
-                name="Total Embeds", value=profile.total_embeds, inline=False
+                name="Total Embeds", value=profile['total_embeds'], inline=False
             )
-        # embed.add_field(name="Top Emojis", value=str(profile.top_emojis), inline=False)
 
         await interaction.followup.send(embed=embed)
 
--- srg_analytics/profile.py
+++ srg_analytics/profile.py
@@ -6,25 +6,6 @@ from srg_analytics.DB import DB
 from srg_analytics.schemas import Profile
 
 
-async def total_message_count(db: DB, guild_id: int, user_id: int) -> int:
-    """Returns the total number of messages in a guild."""
-    return await db.get_message_count(guild_id=guild_id, user_id=user_id)
-
-
-
-
-
-async def get_character_count(db_or_msgs: DB | list[str], guild_id: int, user_id: int) -> int:
-    """Returns the number of characters in a guild."""
-    if isinstance(db_or_msgs, DB):
-        message_content: list[str] = await db_or_msgs.get_message_content(guild_id, user_id or None)
-    else:
-        message_content = db_or_msgs
-
-    # get the number of characters in each message
-    all_messages = "".join(message_content)
-
-    return len(all_messages)
 
 
 async def get_total_attachments(db: DB, guild_id: int, user_id: int) -> int:
@@ -47,53 +28,30 @@ async def get_total_embeds(db: DB, guild_id: int, user_id: int) -> int:
     )[0]
 
 
-async def is_bot(db: DB, guild_id: int, user_id: int) -> bool:
-    """Returns whether a user is a bot or not."""
-    return (
-        await db.execute(
-            f"SELECT is_bot FROM `{guild_id}` WHERE author_id = ? LIMIT 1", 
-            (str(user_id),), fetch="one"
-        )
-    )[0]
-
 async def get_notnull_message_count(db: DB, guild_id: int, user_id: int):
     return (
         await db.execute(
-            f"SELECT count(*) FROM `{guild_id}` WHERE author_id = ? AND message_content IS NOT NULL AND message_content != ''",
+            f"SELECT count(*) FROM `{guild_id}` WHERE author_id = ? AND message_length IS NOT NULL AND message_length != ''",
             (str(user_id),), fetch="one"
         )
     )[0]
-    
 
-async def build_profile(db: DB, guild_id: int, user_id: int) -> Profile:
+
+async def build_profile(db: DB, guild_id: int, user_id: int) -> dict:
     """Builds the profile for a certain user, in a certain guild."""
     start_time = time.time()
 
-    profile = Profile()
+    return_dict = {
+        'user_id': user_id, 'guild_id': guild_id,
+        'messages': await db.get_message_count(guild_id=guild_id, user_id=user_id),
+        'characters': await db.get_character_count(guild_id=guild_id, user_id=user_id),
+        'total_embeds': await get_total_embeds(db, guild_id, user_id),
+        'total_attachments': int(await get_total_attachments(db, guild_id, user_id))
+    }
 
-    profile.user_id = user_id
-    profile.guild_id = guild_id
-    profile.is_bot = await is_bot(db, guild_id, user_id)
+    return_dict['average_message_length'] = return_dict['characters'] / (await get_notnull_message_count(db, guild_id, user_id) or 1)
+    return_dict['time_taken'] = time.time() - start_time
 
+    print(return_dict)
 
-    profile.messages = await total_message_count(db, guild_id, user_id)
-    profile.characters = await get_character_count(msg_list, guild_id, user_id)
-    profile.average_msg_length = profile.characters / await get_notnull_message_count(db, guild_id, user_id)
-
-    profile.total_embeds = await get_total_embeds(db, guild_id, user_id)
-
-    profile.total_attachments = int(await get_total_attachments(db, guild_id, user_id))
-
-    # most_mentioned_ = await most_mentioned(db, guild_id, user_id)
-    # profile.total_mentions = len(most_mentioned_)
-
-    # profile.most_mentioned = most_mentioned_[0]
-    # profile.no_of_times_most_mentioned = most_mentioned_[1]
-
-    # most_mentioned_by_ = await most_mentioned_by(db, guild_id, user_id)
-    # profile.most_mentioned_by = most_mentioned_by_[0]
-    # profile.no_of_times_most_mentioned_by = most_mentioned_by_[1]
-
-    profile.time_taken = time.time() - start_time
-
-    return profile
+    return return_dict
